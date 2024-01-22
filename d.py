44f457df466b815b933ba1170407294c248986ae
PEDRO  PRATES <157413476+PedroPrates08@users.noreply.github.com>
2024-01-22 18:36:35 -0300

    Add files via upload

---
+++ d.py
@@ -0,0 +1,10 @@
+#Crie um dicionário com informações sobre um livro (título, autor, ano de publicação) e depois imprima essas informações.
+
+Jogo = {
+
+    "nome" : "Star Wars - Battlefront",
+    "ano" : 2004,
+    "desenvolvedores" : " DICE, Pandemic Studios, Criterion Games"
+}
+
+print(Jogo)
